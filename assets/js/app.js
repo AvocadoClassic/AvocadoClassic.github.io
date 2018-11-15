a1a1ae0bc4cc8d59ca3bdc4fbbc244a0084d4573
Camilo M. Hernandez <cmilohernandez@gmail.com>
2018-11-15 00:44:41 -0500

    Web3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,29 @@
 //Avocado Classic Inc. Web App
-
-var Web3 = require('web3');
-var web3 = new Web3();
-web3.setProvider(new web3.currentProvider);
+window.addEventListener('load', async () => {
+  // Modern dapp browsers...
+  if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+          // Request account access if needed
+          await ethereum.enable();
+          // Acccounts now exposed
+          web3.eth.sendTransaction({/* ... */});
+      } catch (error) {
+          // User denied account access...
+      }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+      window.web3 = new Web3(web3.currentProvider);
+      // Acccounts always exposed
+      web3.eth.sendTransaction({/* ... */});
+  }
+  // Non-dapp browsers...
+  else {
+      console.log('Non-Ethereum browser detected. This is a web 3.0 dApp. You should consider trying MetaMask!');
+      alert("This is a web 3.0 dApp. You should consider trying MetaMask!");
+  }
+})
 var account = web3.eth.accounts[0];
 var accountInterval = setInterval(function() {
   if (web3.eth.accounts[0] !== account) {
@@ -10,15 +31,3 @@ var accountInterval = setInterval(function() {
     updateInterface();
   }
 }, 100);
-function watchBalance() {
-    var coinbase = web3.eth.coinbase;
-    var originalBalance = web3.eth.getBalance(coinbase).toNumber();
-    document.getElementById('coinbase').innerText = 'coinbase: ' + coinbase;
-    document.getElementById('original').innerText = ' original balance: ' + originalBalance + '    watching...';
-    web3.eth.filter('latest').watch(function() {
-        var currentBalance = web3.eth.getBalance(coinbase).toNumber();
-        document.getElementById("current").innerText = 'current: ' + currentBalance;
-        document.getElementById("diff").innerText = 'diff:    ' + (currentBalance - originalBalance);
-        });
-        
-    }
